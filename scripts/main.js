7ec64bf36796a92f59de7460641ee46148c8a806
Kevin Long <kevin.uu.long@gmail.com>
2021-01-22 07:55:19 -0500

    Simplify

--- scripts/main.js
+++ scripts/main.js
@@ -7,35 +7,39 @@ var screen = document.getElementById("screen");
 
 document.getElementById("recycleBin").addEventListener('click', () => {
     var randomNumber = Math.floor(Math.random() * 12 + 1);
+    appendError(`/images/errors/0${randomNumber}.png`)
+    audio.play();
+})
 
+document.getElementById("ie").addEventListener('click', () => {
+    fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=cat&rating=g`)
+        .then(res => res.json())
+        .then(src => {
+            appendImage(src.data.image_url, src.data.image_width, src.data.image_height);
+        })
+})
+
+function appendGif(src, width, height) {
     var img = new Image();
-    img.src = `/images/errors/0${randomNumber}.png`;
+    img.src = src;
+    img.classList.add('error');
+    var pos = getRandomPosition(width, height);
+    img.style.left = pos[0] + 'px';
+    img.style.top = pos[1] + 'px';
+    screen.appendChild(img);
+}
+
+function appendError(src) {
+    var img = new Image();
+    img.src = src;
     img.classList.add('error');
     var pos = getRandomPosition(img.width, img.height);
     img.style.left = pos[0] + 'px';
     img.style.top = pos[1] + 'px';
     screen.appendChild(img);
-    audio.play();
-})
-
-document.getElementById("ie").addEventListener('click', () => {
-    getImg = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=cat&rating=g`)
-        .then(res => res.json())
-        .then(src => {
-            var imgSrc = src.data.image_url;
-            console.log(src.data);
-            var img = new Image();
-            img.src = imgSrc;
-            img.classList.add('gif');
-            var pos = getRandomPosition(src.data.image_width, src.data.image_height);
-            img.style.left = pos[0] + 'px';
-            img.style.top = pos[1] + 'px';
-            screen.appendChild(img);
-        })
-})
+}
 
 function getRandomPosition(width, height) {
-    console.log(width, height);
     var x = window.innerWidth - width;
     var y = window.innerHeight - height;
     var randomX = Math.floor(Math.random() * x);
